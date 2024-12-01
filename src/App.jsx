03a70c945c3efb608657afc64e824fe3fa80b50a
aekulvir <kulvir.kaur@M-LRWX49655P.local>
2024-12-01 01:27:13 -0800

    menu items push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import Menu from "./Menu";
+import Title from "./Title";
+import menu from "./data";
+import Categories from "./Categories";
+
+const uniqueCategories = ['All', ...new Set(menu.map((item)=> item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(uniqueCategories);
+  const filterItems = (category) => {
+    if (category === 'All') {
+      setMenuItems(menu);
+      return;
+    }
+    const updatedItems = menu.filter((item) => item.category === category);
+    setMenuItems(updatedItems);
+  }
+
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu"/>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
